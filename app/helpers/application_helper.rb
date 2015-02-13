b2963b217eb1cc2c5d1dcaf3b4e0ef99b99051d1
Andre Arko <andre@arko.net>
2015-02-13 17:46:22 +1100

    break out individual pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def subtitle?
+    !@subtitle.nil?
+  end
+
+  def subtitle(text = nil)
+    if text
+      @subtitle = text
+    else
+      @subtitle
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  root "home#index"
+  root "application#index"
+
+  %w[benefits contact join members projects team].each do |page|
+    get "/#{page}" => "home##{page}"
+  end
+
   devise_for :users
 end
