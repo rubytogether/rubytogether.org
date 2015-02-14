5663f55b8e6867b30ec2db7b03605f1a2426d42b
Andre Arko <andre@arko.net>
2015-02-14 23:37:56 +1100

    use minified jquery2 ugh why isn’t it the default

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,6 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
+//= require jquery2.min
 //= require jquery_ujs
 //= require_tree .
