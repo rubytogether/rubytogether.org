40b1a06e3994c573233e999095f1e2dd36eba99b
Andre Arko <andre@arko.net>
2015-02-14 11:46:55 +1100

    force SSL when deployed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  force_ssl if: :ssl_enabled?
+
+  def ssl_enabled?
+    !Rails.env.development?
+  end
 end
