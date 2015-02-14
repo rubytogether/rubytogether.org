a967475fb93e92ca03731e507802cc63e4c8b6e5
Andre Arko <andre@arko.net>
2015-02-14 23:11:15 +1100

    add stripe checkout to join action
    extracts into a dedicated controller, with layout, etc.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   root "application#index"
 
-  %w[benefits contact join members projects team].each do |page|
+  %w[benefits contact members projects team].each do |page|
     get "/#{page}" => "home##{page}"
   end
+
+  get "/join" => "join#index"
 
   devise_for :users
 end
