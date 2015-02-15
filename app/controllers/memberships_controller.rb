79fe1a0b63ae3e53f39dce695195bd794d5263e5
Andre Arko <andre@arko.net>
2015-02-15 23:15:56 +1100

    charge membership fee for one month on demand
    no accounts yet, no subscriptions yet, not tested against stripe yet

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,2 +1,21 @@
+require 'create_membership'
+
 class MembershipsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    token = params.fetch(:token)
+    CreateMembership.new(token).run
+    flash.notice = "Success! Thanks for joining Ruby Together."
+    render plain: membership_url, status: :created
+  rescue CreateMembership::Error => e
+    flash.alert = "Unfortunately, there was an error while charging your card."
+    render plain: join_url, status: :unprocessable_entity
+  end
+
+  def show
+  end
+
 end
---
+++ lib/create_membership.rb
@@ -0,0 +1,58 @@
+class CreateMembership
+  Error = Class.new(RuntimeError)
+
+  attr_reader :token, :email, :amount, :type
+
+  def initialize(params = {})
+    @token  = params.fetch(:id)
+    @email  = params.fetch(:email)
+    @amount = params.fetch(:amount)
+    @type   = membership_type_for(@amount)
+  end
+
+  def run
+    raise Stripe::CardError, "nooo"
+    customer = create_customer(@email, @token)
+    # TODO create user in database, send welcome email
+    create_charge(customer.id, @amount, @type)
+  end
+
+private
+
+  def create_customer(email, token)
+    Stripe::Customer.create(
+      :email => email,
+      :card  => token
+    )
+  end
+
+  def create_charge(id, amount)
+    Stripe::Charge.create(
+      :customer    => id,
+      :amount      => amount,
+      :description => type,
+      :currency    => 'usd'
+    )
+  rescue Stripe::CardError => e
+    debug_error(e)
+    raise Error, "Stripe error #{e.message}"
+  end
+
+  def membership_type_for(amount)
+    case amount
+    when 4000
+      "Individual Member"
+    when 80000
+      "Corporate Member"
+    else
+      raise Error, "unknown membership amount '#{amount}'"
+    end
+  end
+
+  def debug_error(e)
+    # TODO send to exception logger
+    return unless defined?(Rails)
+    Rails.logger.debug(%|#{e.class}: #{e}\n#{e.backtrace.join("\n  ")}|)
+  end
+
+end
