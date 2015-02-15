01b705e43d2fb1cde4fc984cffddab0fdc3aa2f7
Andre Arko <andre@arko.net>
2015-02-15 21:23:00 +1100

    simplify layouts and views dramatically
    move / back to home#index, overwrite header there since it’s the one-off
    move /join into memberships#new, which reveals that it doesn’t need a
    layout at all, only a script tag, so we can remove content_for(:head)

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  root "application#index"
+  root "home#index"
 
   %w[benefits contact members projects team].each do |page|
     get "/#{page}" => "home##{page}"
   end
 
-  get "/join" => "join#index"
+  get "/join" => "memberships#new"
 
   devise_for :users
 end
