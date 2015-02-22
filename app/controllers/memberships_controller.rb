42c265b6014bfcd31bece034446aac9702f85462
Andre Arko <andre@arko.net>
2015-02-22 15:52:13 -0800

    create subscriptions, show results via JS

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -3,22 +3,37 @@ require 'create_membership'
 class MembershipsController < ApplicationController
 
   def create
-    token = Stripe::Token.construct_from(token_params)
-    type  = params.fetch(:type)
+    user = User.where(email: params.fetch(:email)).first_or_create!
+    plan = plan_for(params.fetch(:type))
 
-    CreateMembership.new(token.id, token.email, type).run
+    CreateMembership.run(user, params.fetch(:token), plan)
 
-    flash.notice = "Success! Thanks for joining Ruby Together."
-    render json: {result: "success", url: join_url}
+    notice = "Success! You are now a member of Ruby Together. " \
+      "We've sent you a welcome email with more information."
+    render json: {result: "success", message: notice}
   rescue CreateMembership::Error => e
+    user.destroy if user.stripe_id.nil?
+
     contact_us = self.class.helpers.contact_link("contact us")
     error = "We ran into an error while trying to charge your card. It " \
       "might work if you try again, or you can #{contact_us} for help."
-
     render json: {result: "failure", message: error}
   end
 
   def show
+  end
+
+private
+
+  def plan_for(type)
+    case type
+    when "individual"
+      Stripe::Plans::INDIVIDUAL
+    when "corporate"
+      Stripe::Plans::CORPORATE
+    else
+      raise Error, "unknown membership type #{type.inspect}"
+    end
   end
 
 end
