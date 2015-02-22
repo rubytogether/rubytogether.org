5f594d0684fce58780f96b0839893f651c3b6526
Andre Arko <andre@arko.net>
2015-02-22 13:47:41 -0800

    update stripe config for stripe-rails

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,4 @@
-Rails.configuration.stripe = {
-  :public_key => ENV.fetch("STRIPE_PUBLIC_KEY"),
-  :secret_key => ENV.fetch("STRIPE_SECRET_KEY")
-}
-
-Stripe.api_key = Rails.configuration.stripe[:secret_key]
+Rails.configuration.stripe.tap do |s|
+  s.publishable_key = ENV.fetch("STRIPE_PUBLIC_KEY")
+  s.secret_key = ENV.fetch("STRIPE_SECRET_KEY")
+end
