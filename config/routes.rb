883e105d9b4744c2b2c138e0ed4ffa4bc11b0088
Andre Arko <andre@arko.net>
2015-02-21 18:36:38 -0800

    move the entire static site into HomeController

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  root "application#index"
+  root "home#index"
 
-  %w[benefits contact members projects team].each do |page|
+  %w[benefits contact join members projects team].each do |page|
     get "/#{page}" => "home##{page}"
   end
-
-  get "/join" => "join#index"
 
   devise_for :users
 end
