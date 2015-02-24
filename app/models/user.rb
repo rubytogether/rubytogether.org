35aede705eed156468587d663383becb6a134070
Andre Arko <andre@arko.net>
2015-02-24 14:39:27 -0800

    create memberships
    also move User#member? to Membership#active?

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :recoverable, :rememberable,
     :trackable, :validatable
 
-  def member?
-    member_until && member_until > Time.now
-  end
-
 private
 
   # Normally this method requires a password all the time. In this app,
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150224074426) do
+ActiveRecord::Schema.define(version: 20150224175850) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "memberships", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "name"
+    t.string   "description"
+    t.integer  "type",        default: 0
+    t.datetime "expires_at"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -30,7 +38,6 @@ ActiveRecord::Schema.define(version: 20150224074426) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "stripe_id"
-    t.datetime "member_until"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
