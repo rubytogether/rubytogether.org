7553e1e00ec4ce0984b96c106633feabe86a384c
Andre Arko <andre@arko.net>
2015-02-23 22:56:05 -0800

    add a plans page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "home#index"
 
-  %w[benefits contact join members projects team].each do |page|
+  %w[benefits contact join members plans projects team].each do |page|
     get "/#{page}" => "home##{page}"
   end
 
