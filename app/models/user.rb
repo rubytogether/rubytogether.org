68ad7c4a97b14706699c34ecd68f734e76ea76cb
Andre Arko <andre@arko.net>
2015-02-23 23:47:08 -0800

    keep track of when users are members until
    this means handling the stripe event when a subscription succeeds to
    mark the user as subscribed until the end of that period

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  def member?
+    member_until && member_until > Time.now
+  end
+
 private
 
   # Normally this method requires a password all the time. In this app,
