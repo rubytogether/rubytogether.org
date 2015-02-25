29de9b4868c931b981b3d97ee01f957ee48a2133
Andre Arko <andre@arko.net>
2015-02-24 17:28:48 -0800

    words

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -15,7 +15,7 @@ class MembershipsController < ApplicationController
     user.destroy if user.stripe_id.nil?
 
     contact_us = self.class.helpers.contact_us
-    error = "We ran into an error while trying to charge your card. It " \
+    error = "There was an error while charging your card. It " \
       "might work if you try again, or you can #{contact_us} for help."
     render json: {result: "failure", message: error}
   end
