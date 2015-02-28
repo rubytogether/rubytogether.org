ccb59e5468145f5b93236fc2d6d069505b6efb8e
Andre Arko <andre@arko.net>
2015-02-27 21:14:12 -0800

    render named, active membership into memberships

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,9 @@
 class HomeController < ApplicationController
   after_action :set_cache_control_headers
+
+  def members
+    @corporate = Membership.corporate.named.active.pluck(:name, :description, :url)
+    @individual = Membership.individual.named.active.pluck(:name)
+  end
+
 end
