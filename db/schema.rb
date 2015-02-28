835019f323b8a5604696c33514422101dc2f4c6c
Andre Arko <andre@arko.net>
2015-02-27 21:13:30 -0800

    add URLs for corporate members to link

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150225052107) do
+ActiveRecord::Schema.define(version: 20150227213153) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20150225052107) do
     t.datetime "expires_at"
     t.string   "card_brand"
     t.string   "card_last4"
+    t.string   "url"
   end
 
   create_table "users", force: :cascade do |t|
