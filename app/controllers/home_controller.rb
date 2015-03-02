8dda1514a89369c92972053f9918c27d7a273f81
Andre Arko <andre@arko.net>
2015-03-01 18:29:46 -0800

    cache and purge members page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
   after_action :set_cache_control_headers
 
   def members
+    set_surrogate_key_header("members")
     @corporate = Membership.corporate.named.active.pluck(:name, :description, :url)
     @individual = Membership.individual.named.active.pluck(:name)
   end
--- lib/stripe/invoice/payment_succeeded.rb
+++ lib/stripe/invoice/payment_succeeded.rb
@@ -7,8 +7,9 @@ module Stripe
         customer = Stripe::Customer.retrieve(invoice.customer)
         subscription = customer.subscriptions.retrieve(invoice.subscription)
         expiration = Time.at(subscription.current_period_end)
-        user = User.where(stripe_id: customer.id).first!
+        user = User.where(stripe_id: customer.id).includes(:membership).first!
         user.membership.update_attributes!(expires_at: expiration)
+        FastlyRails.client.purge_by_key("members")
       end
 
     end
