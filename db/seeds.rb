f51c82911c9da0b8bc26b3a76c2a208f7af77484
Andre Arko <andre@arko.net>
2015-03-01 18:30:21 -0800

    remove data before seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,8 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
+[User, Membership].each(&:destroy_all)
+
 directors = [
   "André Arko",
   "Aaron Patterson",
