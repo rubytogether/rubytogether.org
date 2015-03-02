6d88da40771a6cb70711725872c2d8608ad19eaa
Andre Arko <andre@arko.net>
2015-03-01 16:31:53 -0800

    fix the default_url_options for mail previews

--- config/initializers/default_url.rb
+++ config/initializers/default_url.rb
@@ -1,8 +1,13 @@
 require 'uri'
 
-uri = URI.parse ENV.fetch('DEFAULT_URL') 
-
-Rails.application.config.action_mailer.default_url_options = {
+uri = URI.parse ENV.fetch('DEFAULT_URL')
+defaults = {
   host: uri.host,
   port: uri.port
 }
+
+Rails.application.config.action_mailer.default_url_options = defaults
+
+# Apparently mailer previews require this setting on the routes?
+# http://stackoverflow.com/a/24796162/231026
+Rails.application.routes.default_url_options = defaults
