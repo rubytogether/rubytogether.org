2af2102948679527d1e7b8adb6600c65def346dc
Andre Arko <andre@arko.net>
2015-03-01 17:49:38 -0800

    write and style a welcome email

--- spec/mailers/previews/membership_mailer_preview.rb
+++ spec/mailers/previews/membership_mailer_preview.rb
@@ -3,17 +3,17 @@ class MembershipMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/membership_mailer/welcome
   def welcome
-    MembershipMailer.welcome
+    MembershipMailer.welcome(User.first)
   end
 
   # Preview this email at http://localhost:3000/rails/mailers/membership_mailer/goodbye
   def goodbye
-    MembershipMailer.goodbye
+    MembershipMailer.goodbye(User.first)
   end
 
   # Preview this email at http://localhost:3000/rails/mailers/membership_mailer/problem
   def problem
-    MembershipMailer.problem
+    MembershipMailer.problem(User.first)
   end
 
 end
