063c1adf4a03c59592273684fd59eeb004136807
Andre Arko <andre@arko.net>
2015-03-16 14:28:26 -0700

    redirect to membership after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,19 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  # Provides valid CSRF tokens for cached forms
   def csrf
     render json: {
       param: request_forgery_protection_token,
       token: form_authenticity_token
     }
+  end
+
+private
+
+  # Used by Devise to redirect after login
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || membership_path
   end
 
 end
