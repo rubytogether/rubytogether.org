45703cfb3c4f724d58ded7e0ef62c746767b6e80
Andre Arko <andre@arko.net>
2015-03-16 11:54:26 -0700

    update specs to actually, you know, pass

--- spec/controllers/memberships_controller_spec.rb
+++ spec/controllers/memberships_controller_spec.rb
@@ -3,14 +3,12 @@ require "rails_helper"
 RSpec.describe MembershipsController, type: :controller do
   describe "create" do
     let(:user) { double("User") }
-    let(:plan) { double(name: "plan", amount: 100) }
 
     it "runs the membership creator" do
       expect(User).to receive(:where).with(email: "alice@example.com") do
         double(first_or_create!: user)
       end
-      expect(subject).to receive(:plan_for).with("individual") { plan }
-      expect(CreateMembership).to receive(:run).with(user, "abc", plan)
+      expect(CreateMembership).to receive(:run).with(user, "abc", "individual")
 
       post :create, token: "abc", email: "alice@example.com", kind: "individual"
 
