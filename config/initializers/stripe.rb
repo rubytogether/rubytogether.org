794f8caadae693e7263196e927bbc7ad408a290f
Andre Arko <andre@arko.net>
2015-03-17 18:47:18 -0700

    configure stripe and webhook basic auth

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -2,3 +2,12 @@ Rails.configuration.stripe.tap do |s|
   s.publishable_key = ENV.fetch("STRIPE_PUBLIC_KEY")
   s.secret_key = ENV.fetch("STRIPE_SECRET_KEY")
 end
+
+Stripe.api_key = Rails.configuration.stripe.secret_key
+
+# Enable HTTP basic auth if secret is set (for prod but not staging)
+if ENV.has_key?("STRIPE_WEBHOOK_SECRET")
+  StripeEvent.authentication_secret = ENV.fetch("STRIPE_WEBHOOK_SECRET")
+end
+
+require 'stripe/invoice/payment_succeeded'
