e32f70f6215da763dd42c71caf789dd9778bbd5b
Andre Arko <andre@arko.net>
2015-03-19 02:07:59 -0700

    handle payment webhooks from deleted users

--- lib/stripe/invoice/payment_succeeded.rb
+++ lib/stripe/invoice/payment_succeeded.rb
@@ -8,15 +8,21 @@ module Stripe
 
       def call(event)
         invoice = event.data.object
-        customer = Stripe::Customer.retrieve(invoice.customer)
-        subscription = customer.subscriptions.retrieve(invoice.subscription)
-        expiration = Time.at(subscription.current_period_end)
-        user = User.where(stripe_id: customer.id).includes(:membership).first
 
-        if user
-          user.membership.update_attributes!(expires_at: expiration)
-          FastlyRails.client.purge_by_key("members")
+        customer = Stripe::Customer.retrieve(invoice.customer)
+        return if customer.nil? || customer.deleted
+
+        subscription = customer.subscriptions.find do |s|
+          s.id == invoice.subscription
         end
+        return unless subscription
+
+        user = User.where(stripe_id: customer.id).includes(:membership).first
+        return unless user
+
+        expiration = Time.at(subscription.current_period_end)
+        user.membership.update_attributes!(expires_at: expiration)
+        FastlyRails.client.purge_by_key("members")
       end
 
     end
