f6c8ccb5d963cbacf23f6d617d2e2c384dc7c8df
Andre Arko <andre@arko.net>
2015-03-20 22:22:32 -0700

    update news spec for new newses

--- spec/controllers/news_controller_spec.rb
+++ spec/controllers/news_controller_spec.rb
@@ -2,10 +2,9 @@ require 'rails_helper'
 
 RSpec.describe NewsController, type: :controller do
   describe "blog posts" do
-    subject(:response) { get "2015-03-17-announcing-ruby-together" }
+    subject(:response) { get :show, id: "2015-03-17-announcing-ruby-together" }
 
-    it "render" do
-      get "2015-03-17-announcing-ruby-together"
+    it "renders" do
       expect(response.code).to eq("200")
       expect(response).to render_template "news/2015-03-17-announcing-ruby-together"
     end
