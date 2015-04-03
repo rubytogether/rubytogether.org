1248270ac8f8fd158b23fbe926807eb32ccecebe
Andre Arko <andre@arko.net>
2015-04-03 17:58:48 +0100

    stripe event specs with fixtures, yay

--- spec/requests/stripe_events_spec.rb
+++ spec/requests/stripe_events_spec.rb
@@ -4,40 +4,40 @@ RSpec.describe "Stripe webhooks", :vcr do
 
   describe "customer.source.created" do
     it "runs our hook" do
-      user = User.create!(stripe_id: "cus_5uQi8EF4cZQdZR", email: "a@b.c")
+      user = User.create!(stripe_id: "cus_5zX7jk6DxkTfzh", email: "alice@example.com")
       membership = Membership.create(user: user, card_last4: "1234")
 
       expect {
-        post "/stripe/events", id: "evt_15jcmzAcWgwn5pBtGKgreAit"
+        post "/stripe/events", id: "evt_15nY3HAcWgwn5pBtBmDJZBZq"
       }.to change { membership.reload.card_last4 }
     end
 
     context "when user doesn't exist yet" do
       it "returns a 404" do
         expect {
-          post "/stripe/events", id: "evt_15jcmzAcWgwn5pBtGKgreAit"
+          post "/stripe/events", id: "evt_15nY3HAcWgwn5pBtBmDJZBZq"
         }.to raise_error(ActiveRecord::RecordNotFound)
       end
     end
   end
 
   describe "invoice.payment_succeeded" do
-    let(:user) { User.create!(stripe_id: "cus_5vO2rfTHabe64S", email: "a@b.c") }
-    let(:membership) { Membership.create(user: user) }
+    let(:user) { User.create!(stripe_id: "cus_5zX7jk6DxkTfzh", email: "alice@example.com") }
+    let(:membership) { Membership.create(user: user, card_last4: "4242") }
 
     it "runs our hook" do
       expect {
-        post "/stripe/events", id: "evt_15jXGYAcWgwn5pBt2B2eZ6A9"
+        post "/stripe/events", id: "evt_15nY3IAcWgwn5pBtGGpjLCBH"
       }.to change { membership.reload.expires_at }
     end
   end
 
   describe "customer.subscription.created" do
     it "runs our hook" do
-      message = "Subscriber counts: 6 Friend, 49 Individual, and 1 Corporate"
+      message = "Subscriber counts: 1 Friend, 3 Individual, and 4 Corporate"
       options = an_instance_of(Hash)
       expect(Slack).to receive(:say).with(message, options)
-      post "/stripe/events", id: "evt_15jXGYAcWgwn5pBtLeHe5w20"
+      post "/stripe/events", id: "evt_15nY3IAcWgwn5pBtisl4M4d6"
     end
   end
 
