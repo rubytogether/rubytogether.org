3767bc28fc55783c29613bf97c7f9bf158c570bc
Andre Arko <andre@arko.net>
2015-06-30 00:29:00 -0700

    where did all those single quotes come from

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -10,27 +10,27 @@ if ENV.has_key?("STRIPE_WEBHOOK_SECRET")
   StripeEvent.authentication_secret = ENV.fetch("STRIPE_WEBHOOK_SECRET")
 end
 
-require 'membership_plan'
-require 'stripe_event/invoice/payment_succeeded'
-require 'stripe_event/subscription/changed'
-require 'stripe_event/subscription/deleted'
+require "membership_plan"
+require "stripe_event/invoice/payment_succeeded"
+require "stripe_event/subscription/changed"
+require "stripe_event/subscription/deleted"
 
 StripeEvent.configure do |events|
 
-  events.subscribe 'invoice.payment_succeeded',
+  events.subscribe "invoice.payment_succeeded",
     StripeEvent::Invoice::PaymentSucceeded.new
 
-  events.subscribe 'customer.source.created' do |event|
+  events.subscribe "customer.source.created" do |event|
     card = event.data.object
     user_id = User.where(stripe_id: card.customer).pluck(:id).first
     membership = Membership.where(user_id: user_id).first!
     membership.update!(card_brand: card.brand, card_last4: card.last4)
   end
 
-  events.subscribe 'customer.subscription.created',
+  events.subscribe "customer.subscription.created",
     StripeEvent::Subscription::Changed.new
 
-  events.subscribe 'customer.subscription.deleted',
+  events.subscribe "customer.subscription.deleted",
     StripeEvent::Subscription::Deleted.new
 
 end
