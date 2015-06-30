09912306a2a83c1938dd4692a56656d6491000f4
Andre Arko <andre@arko.net>
2015-06-29 20:19:08 -0700

    extract post model

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,44 +1,6 @@
 class NewsController < ApplicationController
   after_action :set_cache_control_headers
 
-  Post = Struct.new(:name, :date, :id) do
-    def self.from_file(file)
-      date = Date.parse(file[0..9])
-      new(file[10..-1], date, file)
-    end
-
-    def self.all
-      glob = Rails.root.join("app/views/news/*")
-      Dir[glob].sort.map do |path|
-        file = File.basename(path, ".html.md")
-        next if file.start_with?("index")
-        from_file(file)
-      end.compact.reverse
-    end
-
-    def self.find!(id)
-      file = id + ".html.md"
-
-      if Rails.root.join("app/views/news", file).exist?
-        Post.from_file(id)
-      else
-        raise ActiveRecord::RecordNotFound
-      end
-    end
-
-    def pathname
-      Rails.root.join("app/views/news", id + ".html.md")
-    end
-
-    def body
-      pathname.read
-    end
-
-    def title
-      name.titleize
-    end
-  end
-
   def index
     @posts = Post.all
   end
