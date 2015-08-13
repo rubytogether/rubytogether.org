7f0dc6d67898f1434ccf3d58b912e68b80251246
Andre Arko <andre@arko.net>
2015-08-12 17:16:17 -0700

    kill flash of non-existent text
    load campton the end of <body> so that the text displays in helvetica if
    necessary to allow visitors to start reading.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,5 +10,6 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w(
   subscribe.js
+  campton.css
   mail.css
 )
