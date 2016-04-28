da7ca4e24c439e40edc43c1b33a01e22b57aec12
Philip Arndt <git@p.arndt.io>
2016-04-29 10:34:30 +1200

    Pull homepage companies from the list of featured companies.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,26 +2,22 @@ class HomeController < ApplicationController
   after_action :set_cache_control_headers
 
   def index
-    @companies = {
-      "Basecamp" => "http://basecamp.com",
-      "Stripe" => "http://stripe.com",
-      "Travis CI" => "https://travis-ci.com",
-      "Airbnb" => "http://airbnb.com",
-      "Yammer" => "http://yammer.com",
-      "GitLab" => "http://gitlab.com",
-      "Kickstarter" => "http://kickstarter.com",
-      "Square" => "http://square.com",
-    }.sort_by { rand }
-    @individuals = Membership.individual.named.order('random()').limit(5).active.pluck(:name)
+    @individuals = Membership.individual.named.order('random()').limit(3).active.pluck(:name)
+    @companies = featured_companies.order('random()').limit(5).pluck(:name, :url)
   end
 
   def members
     set_surrogate_key_header("members")
-    @featured_companies = Membership.featured_companies.named.active.order(:created_at).
+    @featured_companies = featured_companies.order(:created_at).
       pluck(:name, :description, :url)
     @other_companies = Membership.nonfeatured_companies.named.active.order(:created_at).pluck(:name, :url)
     @individual = Membership.individual.named.active.order(:created_at).pluck(:name)
     @individual_count = Membership.individual.count
+  end
+
+  private
+  def featured_companies
+    Membership.featured_companies.named.active
   end
 
 end
