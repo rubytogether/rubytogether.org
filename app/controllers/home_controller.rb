0a93214fa0f7a117449f7dce5bc88a2c9ffc8f0b
Philip Arndt <git@p.arndt.io>
2016-04-29 10:33:46 +1200

    List out companies and individuals on homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,20 @@
 class HomeController < ApplicationController
   after_action :set_cache_control_headers
 
+  def index
+    @companies = {
+      "Basecamp" => "http://basecamp.com",
+      "Stripe" => "http://stripe.com",
+      "Travis CI" => "https://travis-ci.com",
+      "Airbnb" => "http://airbnb.com",
+      "Yammer" => "http://yammer.com",
+      "GitLab" => "http://gitlab.com",
+      "Kickstarter" => "http://kickstarter.com",
+      "Square" => "http://square.com",
+    }.sort_by { rand }
+    @individuals = Membership.individual.named.order('random()').limit(5).active.pluck(:name)
+  end
+
   def members
     set_surrogate_key_header("members")
     @featured_companies = Membership.featured_companies.named.active.order(:created_at).
