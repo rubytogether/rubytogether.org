c573bbb440396a4850fe62edcc254f5cda75bf35
Philip Arndt <git@p.arndt.io>
2016-07-22 15:36:36 +1200

    Add email template for subscriptions that have turned delinquent

--- app/mailers/payment_mailer.rb
+++ app/mailers/payment_mailer.rb
@@ -6,4 +6,10 @@ class PaymentMailer < ApplicationMailer
     mail to: user.email, subject: "Your payment to Ruby Together failed."
   end
 
+  def delinquent(user)
+    @name = user.name.presence || "there" # "Hi Alice," or "Hi there,"
+    @token = user.generate_reset_password_token!
+    mail to: user.email, subject: "Reactivate your Ruby Together subscription?"
+  end
+
 end
