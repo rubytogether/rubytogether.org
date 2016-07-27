1c7566bdfae2b0586f7496a3f81c11da586c7641
Andre Arko <andre@arko.net>
2016-07-26 20:16:24 -0700

    allow setting Stripe API version via ENV

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -5,6 +5,11 @@ end
 
 Stripe.api_key = Rails.configuration.x.stripe_secret_key
 
+# Override Stripe API version to allow testing upgrades
+if ENV.has_key?("STRIPE_API_VERSION")
+  Stripe.api_version = ENV.fetch("STRIPE_API_VERSION")
+end
+
 # Enable HTTP basic auth if secret is set (for prod but not staging)
 if ENV.has_key?("STRIPE_WEBHOOK_SECRET")
   StripeEvent.authentication_secret = ENV.fetch("STRIPE_WEBHOOK_SECRET")
@@ -16,7 +21,6 @@ require "stripe_event/subscription/changed"
 require "stripe_event/subscription/deleted"
 
 StripeEvent.configure do |events|
-
   events.subscribe "invoice.payment_succeeded",
     StripeEvent::Invoice::PaymentSucceeded.new
 
@@ -35,5 +39,4 @@ StripeEvent.configure do |events|
 
   events.subscribe "customer.subscription.deleted",
     StripeEvent::Subscription::Deleted.new
-
 end
