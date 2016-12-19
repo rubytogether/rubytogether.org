b6f87859112e025a6566d398c07f7ec61b880aa9
Andre Arko <andre@arko.net>
2016-12-19 00:26:49 -0800

    make wicked initializer work in test

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,4 +1,3 @@
-require 'wicked_pdf'
 # WickedPDF Global Configuration
 #
 # Use this to set up shared configuration options for your entire application.
@@ -9,14 +8,20 @@ require 'wicked_pdf'
 #
 # https://github.com/mileszs/wicked_pdf/blob/master/README.md
 
-WickedPdf.config = {
-  # Path to the wkhtmltopdf executable: This usually isn't needed if using
-  # one of the wkhtmltopdf-binary family of gems.
-  # exe_path: '/usr/local/bin/wkhtmltopdf',
-  #   or
-  exe_path: Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf')
+begin
+  require 'wicked_pdf'
 
-  # Layout file to be used for all PDFs
-  # (but can be overridden in `render :pdf` calls)
-  # layout: 'pdf.html',
-}
+  WickedPdf.config = {
+    # Path to the wkhtmltopdf executable: This usually isn't needed if using
+    # one of the wkhtmltopdf-binary family of gems.
+    # exe_path: '/usr/local/bin/wkhtmltopdf',
+    #   or
+    exe_path: Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf')
+
+    # Layout file to be used for all PDFs
+    # (but can be overridden in `render :pdf` calls)
+    # layout: 'pdf.html',
+  }
+rescue LoadError
+  # we must not be in development
+end
