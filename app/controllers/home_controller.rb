79f13fedc9c32b4d9530a7e8a7a05609e1279d9b
Andre Arko <andre@arko.net>
2017-02-15 00:00:08 -0800

    group by level on members page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,16 +8,19 @@ class HomeController < ApplicationController
 
   def members
     set_surrogate_key_header("members")
-    @featured_companies = featured_companies.order(:created_at).
-      pluck(:name, :description, :url)
-    @other_companies = Membership.nonfeatured_companies.named.active.order(:created_at).pluck(:name, :url)
+    @featured_levels = levels_for(Membership.featured_companies.named.active.order(:created_at))
+    @other_levels = levels_for(Membership.nonfeatured_companies.named.active.order(:created_at))
     @individual = Membership.individual.named.active.order(:created_at).pluck(:name)
     @individual_count = Membership.individual.active.count
   end
 
-  private
-  def featured_companies
-    Membership.featured_companies.named.active
+private
+
+  def levels_for(companies)
+    levels = companies.all.group_by{|c| c.kind.to_sym }.sort_by{|k,v| MembershipPlan.ids.index(k) }
+    levels.reverse.map do |plan_id, companies|
+      [MembershipPlan[plan_id].shortname, companies.map{|c| [c.name, c.url, c.description] }]
+    end
   end
 
 end
