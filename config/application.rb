4b7d82c3b6464945a4c941e4400d1b9d011fb4f4
Mark Tareshawty <tarebyte@github.com>
2017-06-30 16:35:53 -0400

    Add fonts in assets.rb file

--- config/application.rb
+++ config/application.rb
@@ -33,9 +33,6 @@ module RubyTogether
     # Read the secret key base from ENV
     secrets.secret_key_base = ENV['SECRET_KEY_BASE']
 
-    # Serve fonts from the assets path
-    config.assets.paths << Rails.root.join('app/assets/fonts')
-
     # Automatically reload lib in development
     config.autoload_paths << Rails.root.join('lib')
   end
