d89863f1adc850c6c75fba18e6f3b511492850d6
Andre Arko <andre@arko.net>
2017-07-01 16:20:17 -0700

    include company URLs in since output

--- lib/stats.rb
+++ lib/stats.rb
@@ -13,7 +13,7 @@ class Stats
       map{|plan| [plan, groups[plan.id]] }.
       each do |plan, group|
         message << "#{group.size} new #{plan.shortname} #{"member".pluralize(group.size)}"
-        names = group.map(&:name).compact
+        names = group.map{|m| [m.name, m.url].compact.join(" ") if m.name }.compact
         message << " including\n - #{names.join("\n - ")}" if names.any?
         message << "\n"
       end
