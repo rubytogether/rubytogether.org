58448d2e9cf61145ef166d8bbd0b3ee1fdd58899
Andre Arko <andre@arko.net>
2017-12-11 01:21:11 -0800

    start sending html content type with html

--- app/controllers/invoice_controller.rb
+++ app/controllers/invoice_controller.rb
@@ -8,6 +8,7 @@ class InvoiceController < ApplicationController
     @amount = @membership.plan.amount / 100 * 12
     respond_to do |format|
       format.pdf do
+        response.headers["Content-Type"] = "text/html" if params.has_key?(:debug)
         render show_as_html: params.has_key?(:debug),
           pdf: "#{Date.today.iso8601} Ruby Together Invoice",
           margin: {top: 0, right: 0, bottom: 0, left: 0}
