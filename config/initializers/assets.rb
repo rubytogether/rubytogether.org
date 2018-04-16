028541f07234983a687969fa8f44ecd28126d51b
Adarsh Pandit <adarshpandit@gmail.com>
2018-04-16 15:03:43 -0700

    Run rails app:update

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,13 +4,8 @@
 Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path
-Rails.application.config.assets.paths << Rails.root.join('app/assets/fonts')
+# Rails.application.config.assets.paths << Emoji.images_path
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w(
-  subscribe.js
-  campton.css
-  mail.css
-  invoice.css
-)
+# Rails.application.config.assets.precompile += %w( search.js )
