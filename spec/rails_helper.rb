709adc0640ee01c9f068d9e10265b1a687abd46a
Andre Arko <andre@arko.net>
2018-04-18 18:58:53 -0400

    remove some RSpec config we don't use

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -24,13 +24,7 @@ require 'rspec/rails'
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
-RSpec.configure do |config|
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
   config.use_transactional_fixtures = true
 
   config.include Devise::Test::ControllerHelpers, type: :controller
