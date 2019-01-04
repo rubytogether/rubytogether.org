e3f6c5af60f805548283af3817a2e43ccb8e86f7
Andre Arko <andre@arko.net>
2019-01-03 21:34:13 -0800

    Update schema for Rails 5.2

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,47 +10,47 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160815025629) do
+ActiveRecord::Schema.define(version: 2018_05_14_084408) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "memberships", force: :cascade do |t|
-    t.integer  "user_id"
-    t.string   "name"
-    t.string   "description"
-    t.integer  "kind",          default: 0
+  create_table "memberships", id: :serial, force: :cascade do |t|
+    t.integer "user_id"
+    t.string "name"
+    t.string "description"
+    t.integer "kind", default: 0
     t.datetime "expires_at"
-    t.string   "card_brand"
-    t.string   "card_last4"
-    t.string   "url"
-    t.string   "twitter"
-    t.string   "contact_name"
-    t.string   "contact_phone"
-    t.string   "logo_url"
-    t.string   "contact_email"
+    t.string "card_brand"
+    t.string "card_last4"
+    t.string "url"
+    t.string "twitter"
+    t.string "contact_name"
+    t.string "contact_phone"
+    t.string "logo_url"
+    t.string "contact_email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.datetime "cancelled_at"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "email",                           default: "", null: false
-    t.string   "encrypted_password",              default: "", null: false
-    t.string   "reset_password_token"
+  create_table "users", id: :serial, force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                   default: 0,  null: false
+    t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "stripe_id"
+    t.string "stripe_id"
     t.datetime "delinquent_notification_sent_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
