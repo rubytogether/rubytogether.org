92b871adc617d40e02d7e63e518ed2815c56e1d7
Andre Arko <andre@arko.net>
2019-01-23 23:21:36 -0800

    stop trying to set the (removed) webhook secret

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -9,11 +9,6 @@ if ENV.has_key?("STRIPE_API_VERSION")
   Stripe.api_version = ENV.fetch("STRIPE_API_VERSION")
 end
 
-# Enable HTTP basic auth if secret is set (for prod but not staging)
-if ENV.has_key?("STRIPE_WEBHOOK_SECRET")
-  StripeEvent.authentication_secret = ENV.fetch("STRIPE_WEBHOOK_SECRET")
-end
-
 # Stripe now provides a signing secret for webhooks, hooray
 if ENV.has_key?("STRIPE_SIGNING_SECRET")
   StripeEvent.signing_secret = ENV['STRIPE_SIGNING_SECRET']
