9011378f3a111a315c90a22b67af114a40ccbc81
Andre Arko <andre@arko.net>
2019-04-14 00:55:09 -0700

    stop sending 404s to rollbar

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -19,7 +19,7 @@ Rollbar.configure do |config|
   # via the rollbar interface.
   # Valid levels: 'critical', 'error', 'warning', 'info', 'debug', 'ignore'
   # 'ignore' will cause the exception to not be reported at all.
-  # config.exception_level_filters.merge!('MyCriticalException' => 'critical')
+  config.exception_level_filters.merge!("ActionController::RoutingError" => "ignore")
   #
   # You can also specify a callable, which will be called with the exception instance.
   # config.exception_level_filters.merge!('MyCriticalException' => lambda { |e| 'critical' })
