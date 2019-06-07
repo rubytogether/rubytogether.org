cc86f7ed0494239abf93e71b9175df8a73e2ef19
Andre Arko <andre@arko.net>
2019-06-07 15:10:16 -0700

    only list 3 companies on the homepage, max

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,9 +4,8 @@ class HomeController < ApplicationController
   def index
     @developers = Membership.developer.named.active.order('random()').limit(3).pluck(:name)
     @companies = [
-      Membership.plan(:ruby).named.active.order(:created_at).pluck(:name, :url),
-      Membership.plan(:sapphire).named.active.order(:created_at).pluck(:name, :url),
-      Membership.plan(:emerald).named.active.order('random()').limit(2).pluck(:name, :url)
+      Membership.plan(:ruby).named.active.order('random()').limit(2).pluck(:name, :url),
+      Membership.plan(:sapphire).named.active.order('random()').limit(1).pluck(:name, :url)
     ].flatten(1)
   end
 
