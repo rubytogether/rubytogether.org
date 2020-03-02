35c5c7724356136466cc380e53cfe7469153e713
Andre Arko <andre@arko.net>
2020-03-01 22:03:19 -0700

    prevent some possible exceptions

--- app/lib/stripe_event/subscription/changed.rb
+++ app/lib/stripe_event/subscription/changed.rb
@@ -6,6 +6,7 @@ module StripeEvent
         user = user_for_event(event)
         sub = user_subscription_for_event(user, event)
 
+        return unless sub&.plan&.product
         level = MembershipProduct.find_by_stripe_id(sub.plan.product).id
         user&.membership&.update(
           interval: sub.plan.interval,
