d88e0a02a5d51bbce20551f7c2e06e453221ec0f
Andre Arko <andre@arko.net>
2020-03-09 22:54:37 -0700

    handle missing users and products without 500s

--- app/lib/stripe_event/subscription/changed.rb
+++ app/lib/stripe_event/subscription/changed.rb
@@ -4,13 +4,17 @@ module StripeEvent
 
       def call(event)
         user = user_for_event(event)
-        sub = user_subscription_for_event(user, event)
+        return unless user
 
+        sub = user_subscription_for_event(user, event)
         return unless sub&.plan&.product
-        level = MembershipProduct.find_by_stripe_id(sub.plan.product).id
-        user&.membership&.update(
+
+        product = MembershipProduct.find_by_stripe_id(sub.plan.product)
+        return unless product
+
+        user.membership&.update(
           interval: sub.plan.interval,
-          level: level
+          level: product.id
         )
       end
 
