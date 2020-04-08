6bac948787887cae56edfb47787bad3602ec06e0
Andre Arko <andre@arko.net>
2020-04-07 23:59:05 -0700

    finish Rails 6 upgrade

--- config/application.rb
+++ config/application.rb
@@ -8,12 +8,10 @@ Bundler.require(*Rails.groups)
 
 module RubyTogether
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.0
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.eager_load_paths += %W(#{config.root}/lib)
   end
 end
--- config/initializers/markdown_handler.rb
+++ config/initializers/markdown_handler.rb
@@ -5,8 +5,8 @@ module MarkdownHandler
     @erb ||= ActionView::Template.registered_template_handler(:erb)
   end
 
-  def self.call(template)
-    compiled_source = erb.call(template)
+  def self.call(template, source)
+    compiled_source = erb.call(template, source)
     "RDiscount.new(begin;#{compiled_source};end).to_html"
   end
 end
