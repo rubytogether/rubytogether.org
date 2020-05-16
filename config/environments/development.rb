75976673216d5742b687fd60f10c5dbc2ce1e861
Andre Arko <andre@arko.net>
2020-05-15 23:50:46 -0700

    allow puma-dev in development hosts

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -56,4 +56,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Allow puma-dev to run at rubytogether.test
+  config.hosts << "rubytogether.test"
 end
