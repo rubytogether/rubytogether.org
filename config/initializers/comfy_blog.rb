d6362e566faaccaa261b2c8cb883e665a59f832a
Andre Arko <andre@arko.net>
2020-07-11 19:54:20 -0700

    set blog index layout, monkeypatch in date method

--- config/initializers/comfy_blog.rb
+++ config/initializers/comfy_blog.rb
@@ -2,8 +2,14 @@
 
 ComfyBlog.configure do |config|
   # application layout to be used to index blog posts
-  #   config.app_layout = 'comfy/blog/application'
+  config.app_layout = 'application'
 
   # Number of posts per page. Default is 10
   #   config.posts_per_page = 10
+end
+
+class Comfy::Blog::Post < ActiveRecord::Base
+  def date
+    published_at.to_date
+  end
 end
