27ee5b932e46e0a1c892f617cad62505a3e35d41
Andre Arko <andre@arko.net>
2020-07-12 01:41:43 -0700

    make blog post content markdown, import blog posts in migration

---
+++ db/migrate/20200712065639_import_blog_posts.rb
@@ -0,0 +1,18 @@
+class ImportBlogPosts < ActiveRecord::Migration[6.0]
+  def change
+    Post.all.reverse.each do |np|
+      md = NewsController.render(inline: np.body)
+
+      ::Comfy::Blog::Post.create_with(
+        created_at: np.date,
+        published_at: np.date,
+        site: ::Comfy::Cms::Site.find_by(identifier: "ruby-together"),
+        layout: ::Comfy::Cms::Layout.find_by(identifier: "news"),
+        title: np.title,
+        fragments_attributes: [
+          {identifier: "summary", content: np.summary},
+          {identifier: "content", content: md},
+        ]).find_or_create_by!(slug: np.name)
+    end
+  end
+end
