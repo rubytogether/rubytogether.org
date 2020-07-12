1e29d7cb343e8581e770c93fef5da7c41ba4a803
Andre Arko <andre@arko.net>
2020-07-12 14:27:08 -0700

    create a site before importing into it

--- db/migrate/20200712065638_import_site_content.rb
+++ db/migrate/20200712065638_import_site_content.rb
@@ -1,5 +1,11 @@
 class ImportSiteContent < ActiveRecord::Migration[6.0]
   def change
+    Comfy::Cms::Site.create!(
+      label: "Ruby Together",
+      identifier: "ruby-together",
+      hostname: Rails.application.routes.default_url_options.fetch(:host),
+      locale: "en"
+    )
     ComfortableMexicanSofa::Seeds::Importer.new("ruby-together", "ruby-together").import!
   end
 end
