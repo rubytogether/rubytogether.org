c52298d79c2c0ddeccec691776ea31319a6cc548
Andre Arko <andre@arko.net>
2021-02-15 15:39:27 -0800

    fix slack-notifier for ruby 3

--- config/initializers/slack.rb
+++ config/initializers/slack.rb
@@ -7,5 +7,30 @@ end
 
 if ENV.has_key?("SLACK_WEBHOOK_URL")
   require "slack-notifier"
+
+  # https://github.com/stevenosloan/slack-notifier/issues/124#issuecomment-776169011
+  module Slack
+    class Notifier
+      module Util
+        class LinkFormatter
+
+          # Outmoded for Ruby 3.0.0
+          # def initialize string, formats: %i[html markdown]
+          #   @formats = formats
+          #   @orig    = string.respond_to?(:scrub) ? string.scrub : string
+          # end
+
+          # Monkey patch for Ruby 3.0.0
+          def initialize string, in_opts={formats: %i[html markdown]}
+            opts = in_opts || {}
+            @formats = opts[:formats]
+            @orig    = string.respond_to?(:scrub) ? string.scrub : string
+          end
+
+        end
+      end
+    end
+  end
+
   Slack.notifier = Slack::Notifier.new(ENV.fetch("SLACK_WEBHOOK_URL"))
 end
